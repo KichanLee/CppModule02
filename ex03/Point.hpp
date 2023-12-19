dfc431350a383785baefe5fdb186abe9e5ce2a13
KichanLee <lkc0847@naver.com>
2023-12-20 05:21:55 +0900

    1220

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: kichan <kichan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:14:52 by kichlee           #+#    #+#             */
-/*   Updated: 2023/12/19 14:56:50 by kichan           ###   ########.fr       */
+/*   Updated: 2023/12/20 04:47:49 by kichan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ class Point {
   Fixed getFixedValueX(void) const;
   Fixed getFixedValueY(void) const;
 };
+
 float area(Point a, Point b, Point c);
 
 bool bsp(Point const a, Point const b, Point const c, Point const point);
